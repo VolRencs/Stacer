799285eed8e37f04e92f2e30b927285b41ada19f
QuentiumYT <quentin.lienhardt@gmail.com>
2025-06-20 16:18:19 +0200

    Use Debug type in build script

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 rm -rf build
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build -j $(nproc)
-strip -s build/output/stacer build/output/lib/libstacer-core.a
 
 ./build/output/stacer
